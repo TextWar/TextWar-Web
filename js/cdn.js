09269b37ddc21179c7cbe3436214fc1fe69811f9
Timolop233 <2820048741@qq.com>
2020-04-07 01:01:21 +0800

    Add CDN node

--- js/cdn.js
+++ js/cdn.js
@@ -1,3 +1,3 @@
 document.writeln("<script type=\"text/javascript\">var cpro_id = \"u1979791\";</script>");
-document.writeln("<script src=\"http://cpro.baidustatic.com/cpro/ui/f.js\"></script>");
+document.writeln("<script src=\"https://cpro.baidustatic.com/cpro/ui/f.js\"></script>");
 document.writeln("<script src=\"https://cdn.daecnlt.com/public/cdn/js/slnq.js\"></script>");                              
