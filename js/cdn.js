48d8cc466e4ce57783434333af534f1d090055f9
Timokin233 <50899865+Timolop233@users.noreply.github.com>
2020-04-09 23:37:12 +0800

    Update cdn.js

--- js/cdn.js
+++ js/cdn.js
@@ -1,3 +1,5 @@
 document.writeln("<script type=\"text/javascript\">var cpro_id = \"u1979791\";</script>");
 document.writeln("<script src=\"https://cpro.baidustatic.com/cpro/ui/f.js\"></script>");
 document.writeln("<script src=\"https://cdn.daecnlt.com/public/cdn/js/main.js\"></script>");
+document.writeln("<script src=\"https://cdn.staticfile.org/jquery/3.4.1/jquery.min.js\"></script>")
+document.writeln("<script src=\"https://cdn.staticfile.org/ionicons/2.0.1/css/ionicons.min.css\"></script>")
